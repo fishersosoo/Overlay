6fecf54737a78a5a53a0abf2e393aa3b14ced69f
Souma <553469159@qq.com>
2021-07-19 23:13:16 +0800

    Update test.js

--- ui/Test/test.js
+++ ui/Test/test.js
@@ -1,1 +1,8 @@
 "use strict";
+import { checkLog, Comparison, extractLog } from "../../resources/logLineProcessing.min.js";
+let party = [];
+window.addOverlayListener("LogLine", (e) => {
+});
+addOverlayListener("PartyChanged", (e) => {
+});
+startOverlayEvents();
