a53b4b887cad39f0a30d7c30642b90631a0c314d
Souma <553469159@qq.com>
2021-07-24 15:15:21 +0800

    文字显示加载时未更新

--- ui/TeamWatch/settingStyle.js
+++ ui/TeamWatch/settingStyle.js
@@ -1,5 +1,6 @@
 "use strict";
 $("#bgOpacity").val(localStorage.getItem("setBgOpacity") || 0.5);
+$("#text").text($("#bgOpacity").val());
 window.save = function () {
   localStorage.setItem("setBgOpacity", $("#bgOpacity").val());
 };
