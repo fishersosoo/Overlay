ae74a2fab833ae58020e8255d8bfabaff9f5d19e
Souma <553469159@qq.com>
2021-09-17 16:25:20 +0800

    2021.09.17 悬浮窗 减伤记录2

---
+++ ui/KeigennRecord2/data/keigenn.js
@@ -0,0 +1,70 @@
+/*
+ * @Author: Souma
+ * @LastEditTime: 2021-09-16 13:24:13
+ */
+let keigenn = {
+  "4a7": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //铁壁
+  "740": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //盾阵
+  "496": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //干预
+  "4a": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //预警
+  "498": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //武装
+  "52": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //神圣领域
+  "2df": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //原初的直觉
+  "8b3": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //原初的勇猛
+  "57": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //战栗
+  "59": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //复仇
+  "199": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //死斗
+  "49a": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //至黑之夜
+  "2ea": { dodge: 1, physics: 0, magic: 1, darkness: 0, heal: 1 }, //弃明投暗
+  "2eb": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //暗影墙
+  "766": { dodge: 1, physics: 0, magic: 1, darkness: 0, heal: 1 }, //暗黑布道
+  "32a": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //行尸走肉
+  "811": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //死而不僵
+  "730": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //石之心
+  "728": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //伪装
+  "72a": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //星云
+  "72f": { dodge: 1, physics: 0, magic: 1, darkness: 0, heal: 1 }, //光之心
+  "72c": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //超火流星
+  "751": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //节制
+  "12b": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //野战治疗阵
+  "13d": { dodge: 1, physics: 0, magic: 1, darkness: 0, heal: 1 }, //异想的幻光
+  "753": { dodge: 1, physics: 0, magic: 1, darkness: 0, heal: 1 }, //炽天的幻光
+  "351": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //命运之轮（日）
+  "4b6": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //命运之轮（夜）
+
+  "4d0": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //心眼
+  "49b": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //金刚极意
+  "78e": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //行吟
+  "79f": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //策动
+  "722": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //防守之桑巴
+
+  "2d7": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //圣光幕帘（已触发）
+  "4c2": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //神祝祷
+  "5b1": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //摆脱
+  "129": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //鼓舞
+  "77d": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //炽天的幕帘
+  "345": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //黑夜领域
+  "758": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //天星冲日（夜）
+  "761": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //天星交错（夜）
+  "1e8": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //残影
+  "a8": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //魔罩
+  "790": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //防护障壁
+  "76a": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //残暴弹
+
+  "4b9": { dodge: 1, physics: 1, magic: 0, darkness: 0, heal: 1 }, //亲疏自行
+  "4a9": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //雪仇
+  "4ab": { dodge: 1, physics: 1, magic: 0, darkness: 0, heal: 1 }, //牵制
+  "4b3": { dodge: 1, physics: 0, magic: 1, darkness: 0, heal: 1 }, //昏乱
+
+  //青魔
+  "848": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //体力增加（捕食）
+  "6b3": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //腐臭（臭气）
+  "843": { dodge: 1, physics: 0, magic: 1, darkness: 0, heal: 1 }, //智力精神降低（魔法锤）
+  "9c4": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //龙之力
+  "6ba": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //超硬化
+  "9c0": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //玄结界
+  "847": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //仙人盾
+  "6b7": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //强力守护
+  "842": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //哥布防御
+};
+export { keigenn };
---
+++ ui/KeigennRecord2/js/index.js
@@ -0,0 +1,151 @@
+/*
+ * @Author: Souma
+ * @LastEditTime: 2021-09-17 16:15:42
+ */
+"use strict";
+
+import { jobList } from "../../../resources/data/job.js";
+import { status } from "../../../resources/data/status.js";
+import { logProcessing } from "../../../resources/data/logProcessing.js";
+import { getDamage } from "../../../resources/function/damage.js";
+// import { loadItem, saveItem } from "../../../resources/localStorage.min.js";
+import { keigenn } from "../data/keigenn.js";
+let party = [],
+  youID = "",
+  duration = "00:00",
+  FFXIVObject = {},
+  scrollMove = true;
+class FFObject {
+  constructor(id, name) {
+    this.ID = id;
+    this.Name = name;
+    this.Status = {};
+  }
+}
+function addFooter() {
+  document.querySelector("body > footer > ul").innerHTML = `<li class="select" data-object-id="${youID}" data-select="true" data-job-name="YOU" data-reality-name="YOU">YOU</li>`;
+  if (party.length) {
+    document.querySelector("body > footer > ul").append(
+      ...party
+        .filter((p) => p.id !== youID)
+        .map((value) => {
+          let li = document.createElement("li");
+          li.setAttribute("data-reality-name", value.name);
+          li.setAttribute("data-job-name", (jobList.find((job) => job.ID.toString() === value.job.toString()) || { cn: "未知" }).cn);
+          li.appendChild(document.createTextNode(li.getAttribute("data-job-name")));
+          li.setAttribute("data-object-id", value.id);
+          li.setAttribute("data-select", "false");
+          return li;
+        })
+    );
+  }
+  document.querySelectorAll("body > footer > ul > li ").forEach((li) => {
+    li.onclick = function () {
+      document.querySelectorAll("body > footer > ul > li ").forEach((li) => {
+        li.setAttribute("data-select", "false");
+        li.classList.remove("select");
+      });
+      this.setAttribute("data-select", "true");
+      li.classList.add("select");
+      document
+        .querySelectorAll("body > main > table > tbody > tr:not(:nth-child(1))")
+        .forEach((element) => (element.style.display = element.getAttribute("data-master") === li.getAttribute("data-object-id") ? "table-row" : "none"));
+    };
+    li.oncontextmenu = () =>
+      document
+        .querySelectorAll("body > footer > ul > li ")
+        .forEach((li) => (li.innerText = li.innerText === li.getAttribute("data-reality-name") ? li.getAttribute("data-job-name") : li.getAttribute("data-reality-name")));
+  });
+}
+
+addOverlayListener("ChangePrimaryPlayer", (e) => {
+  youID = e.charID.toString(16).toUpperCase();
+  addFooter();
+});
+
+addOverlayListener("PartyChanged", (e) => {
+  party = e.party.filter((p) => p.inParty);
+  addFooter();
+});
+
+addOverlayListener("CombatData", (e) => (duration = e.Encounter.duration));
+
+addOverlayListener("ChangeZone", () => {
+  FFXIVObject = {};
+  document.querySelector("body > main > table > tbody").innerHTML = "<tr></tr>";
+});
+
+addOverlayListener("onPartyWipe", () => {
+  FFXIVObject = {};
+  document.querySelector("body > main > table > tbody").insertRow(-1).insertCell(0).innerHTML = "团灭";
+});
+
+addOverlayListener("LogLine", (e) => {
+  let l;
+
+  function relevantAction() {
+    return l["casterID"].substring(0, 1) === "4" && (party.some((value) => value.id === l["targetID"] && value.inParty) || l["targetID"] === youID);
+  }
+
+  function relevantStatus() {
+    return party.some((value) => value.id === l["targetID"] && value.inParty) || l["targetID"] === youID || l["targetID"].substring(0, 1) === "4";
+  }
+
+  switch (e.line[0]) {
+    case "21":
+    case "22":
+      l = logProcessing(e.line, "action");
+      if (relevantAction(e.line)) {
+        let row = document.querySelector("body > main > table > tbody").insertRow(-1);
+        row.setAttribute("data-master", l["targetID"]);
+        row.style.display = document.querySelector(`body > footer > ul > li[data-object-id="${l["targetID"]}"]`).getAttribute("data-select") === "true" ? "table-row" : "none";
+        let damage = getDamage(e);
+        row.insertCell(0).innerHTML = duration;
+        row.insertCell(1).innerHTML = l["actionName"];
+        let cell3 = row.insertCell(2);
+        cell3.innerHTML = damage.value;
+        cell3.setAttribute("data-damage-effect", damage.damageEffect);
+        cell3.title = damage.from;
+        cell3.classList.add(damage.damageType);
+        let cell4 = row.insertCell(3);
+
+        function createImg(type, key, damage, cell4) {
+          let img = document.createElement("img");
+          img.setAttribute("src", `https://cafemaker.wakingsands.com/i/${status[parseInt(key, 16)].url}`);
+          img.title = FFXIVObject[l[type]].Status[key].name;
+          if (keigenn[key][damage.damageType] === "0") img.classList.add("useless");
+          cell4.appendChild(img);
+        }
+        if (FFXIVObject[l["targetID"]]) for (const key in FFXIVObject[l["targetID"]].Status) createImg("targetID", key, damage, cell4);
+        if (FFXIVObject[l["casterID"]]) for (const key in FFXIVObject[l["casterID"]].Status) createImg("casterID", key, damage, cell4);
+        if (scrollMove && document.querySelector(`body > footer > ul > li[data-object-id="${l["targetID"]}"]`).getAttribute("data-select") === "true") {
+          document.querySelector("body > main").scrollTop = document.querySelector("body > main").scrollHeight;
+        }
+      }
+      break;
+    case "26":
+      l = logProcessing(e.line, "status");
+      if (keigenn[l["statusID"]] && relevantStatus(e.line)) {
+        FFXIVObject[l["targetID"]] = FFXIVObject[l["targetID"]] || new FFObject(l["targetID"], l["targetName"]);
+        FFXIVObject[l["targetID"]].Status[l["statusID"]] = {
+          name: l["statusName"],
+          caster: l["casterName"],
+        };
+      }
+      break;
+    case "30":
+      l = logProcessing(e.line, "status");
+      if (keigenn[l["statusID"]] && relevantStatus(e.line)) {
+        try {
+          delete FFXIVObject[l["targetID"]].Status[l["statusID"]];
+        } catch {}
+      }
+      break;
+    default:
+      break;
+  }
+});
+
+startOverlayEvents();
+
+document.querySelector("main").onscroll = (e) => (scrollMove = document.querySelector("main").scrollHeight - 282 - e.target.scrollTop < 5);
