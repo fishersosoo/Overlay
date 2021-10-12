f6106fafd9a38810786257d29d670f663bbba389
Souma <553469159@qq.com>
2021-10-12 11:38:48 +0800

    2021.10.12

--- ui/KeigennRecord2/js/index.js
+++ ui/KeigennRecord2/js/index.js
@@ -1,6 +1,6 @@
 /*
  * @Author: Souma
- * @LastEditTime: 2021-10-05 14:39:01
+ * @LastEditTime: 2021-10-12 11:37:38
  */
 "use strict";
 import { jobList } from "../../../resources/data/job.js";
@@ -12,7 +12,9 @@ let party = [],
   youID = "",
   duration = "00:00",
   FFXIVObject = {},
-  scrollMove = true;
+  scrollMove = true,
+  inCombat = false,
+  combatTimer = 0;
 class FFObject {
   constructor(id, name) {
     this.ID = id;
@@ -29,7 +31,9 @@ function getUrlParam(name) {
 document.querySelector("main").style.backgroundColor = `rgba(5,5,5,${getUrlParam("bgOpacity") || 0.45})`;
 document.body.style.opacity = getUrlParam("bodyOpacity") || 1;
 function addFooter() {
-  document.querySelector("body > footer > ul").innerHTML = `<li class="select" data-object-id="${youID}" data-select="true" data-job-name="YOU" data-reality-name="YOU">YOU</li>`;
+  document.querySelector(
+    "body > footer > ul"
+  ).innerHTML = `<li class="select" data-object-id="${youID}" data-select="true" data-job-name="YOU" data-reality-name="YOU">YOU</li>`;
   if (party.length) {
     document.querySelector("body > footer > ul").append(
       ...party
@@ -37,7 +41,10 @@ function addFooter() {
         .map((value) => {
           let li = document.createElement("li");
           li.setAttribute("data-reality-name", value.name);
-          li.setAttribute("data-job-name", (jobList.find((job) => job.ID.toString() === value.job.toString()) || { cn: "未知" }).cn);
+          li.setAttribute(
+            "data-job-name",
+            (jobList.find((job) => job.ID.toString() === value.job.toString()) || { cn: "未知" }).cn
+          );
           li.appendChild(document.createTextNode(li.getAttribute("data-job-name")));
           li.setAttribute("data-object-id", value.id);
           li.setAttribute("data-select", "false");
@@ -55,12 +62,22 @@ function addFooter() {
       li.classList.add("select");
       document
         .querySelectorAll("body > main > table > tbody > tr:not(:nth-child(1))")
-        .forEach((element) => (element.style.display = element.getAttribute("data-master-id") === li.getAttribute("data-object-id") ? "table-row" : "none"));
+        .forEach(
+          (element) =>
+            (element.style.display =
+              element.getAttribute("data-master-id") === li.getAttribute("data-object-id") ? "table-row" : "none")
+        );
     };
     li.oncontextmenu = () =>
       document
         .querySelectorAll("body > footer > ul > li ")
-        .forEach((li) => (li.innerText = li.innerText === li.getAttribute("data-reality-name") ? li.getAttribute("data-job-name") : li.getAttribute("data-reality-name")));
+        .forEach(
+          (li) =>
+            (li.innerText =
+              li.innerText === li.getAttribute("data-reality-name")
+                ? li.getAttribute("data-job-name")
+                : li.getAttribute("data-reality-name"))
+        );
   });
 }
 addOverlayListener("ChangePrimaryPlayer", (e) => {
@@ -71,12 +88,25 @@ addOverlayListener("PartyChanged", (e) => {
   party = e.party.filter((p) => p.inParty);
   addFooter();
 });
-addOverlayListener("CombatData", (e) => (duration = e.Encounter.duration));
-addOverlayListener("ChangeZone", () => (FFXIVObject = {}), (document.querySelector("body > main > table > tbody").innerHTML = "<tr></tr>"));
+try {
+  addOverlayListener("onInCombatChangedEvent", (e) => (e.detail.inACTCombat && !inCombat ? startCombat() : ""));
+} catch {
+  addOverlayListener("CombatData", (e) => (duration = e.Encounter.duration));
+}
+addOverlayListener("ChangeZone", () => {
+  FFXIVObject = {};
+  document.querySelector("body > main > table > tbody").innerHTML = "<tr></tr>";
+  inCombat = false;
+  clearTimeout(combatTimer);
+  duration = "00:00";
+});
 addOverlayListener("onPartyWipe", () => {
   FFXIVObject = {};
   document.querySelector("body > main > table > tbody").insertRow(-1).insertCell(0).innerHTML = "团灭";
   document.querySelector("body > main").scrollTop = document.querySelector("body > main").scrollHeight;
+  inCombat = false;
+  clearTimeout(combatTimer);
+  duration = "00:00";
 });
 addOverlayListener("LogLine", (e) => {
   let l = {};
@@ -84,11 +114,20 @@ addOverlayListener("LogLine", (e) => {
     case "21":
     case "22":
       l = logProcessing(e.line, "action");
-      if (l["casterID"].substring(0, 1) === "4" && (party.some((value) => value.id === l["targetID"] && value.inParty) || l["targetID"] === youID)) {
+      if (
+        l["casterID"].substring(0, 1) === "4" &&
+        (party.some((value) => value.id === l["targetID"] && value.inParty) || l["targetID"] === youID)
+      ) {
+        if (!inCombat && duration === "00:00") startCombat();
         let row = document.querySelector("body > main > table > tbody").insertRow(-1);
         row.setAttribute("data-master-id", l["targetID"]);
         row.setAttribute("data-master-name", l["targetName"]);
-        row.style.display = document.querySelector(`body > footer > ul > li[data-object-id="${l["targetID"]}"]`).getAttribute("data-select") === "true" ? "table-row" : "none";
+        row.style.display =
+          document
+            .querySelector(`body > footer > ul > li[data-object-id="${l["targetID"]}"]`)
+            .getAttribute("data-select") === "true"
+            ? "table-row"
+            : "none";
         let damage = getDamage(e);
         row.insertCell(0).innerHTML = duration;
         row.insertCell(1).innerHTML = l["actionName"];
@@ -107,7 +146,12 @@ addOverlayListener("LogLine", (e) => {
         }
         if (FFXIVObject[l["targetID"]]) for (const key in FFXIVObject[l["targetID"]].Status) createImg("targetID", key);
         if (FFXIVObject[l["casterID"]]) for (const key in FFXIVObject[l["casterID"]].Status) createImg("casterID", key);
-        if (scrollMove && document.querySelector(`body > footer > ul > li[data-object-id="${l["targetID"]}"]`).getAttribute("data-select") === "true") {
+        if (
+          scrollMove &&
+          document
+            .querySelector(`body > footer > ul > li[data-object-id="${l["targetID"]}"]`)
+            .getAttribute("data-select") === "true"
+        ) {
           document.querySelector("body > main").scrollTop = document.querySelector("body > main").scrollHeight;
         }
         row.onclick = () => {
@@ -125,8 +169,14 @@ addOverlayListener("LogLine", (e) => {
           document.querySelector("#hint").classList.add("anim-opacity2");
           setTimeout(() => document.querySelector("#hint").classList.remove("anim-opacity2"), 2000);
         };
-      } else if ((party.some((value) => value.id === l["targetID"] && value.inParty) || l["targetID"] === youID) && keigenn[l["actionID"]] !== undefined) {
-        document.querySelector("body > main > table > tbody").insertRow(-1).insertCell(0).innerHTML = `${duration} ${l["casterName"]}发动了“${l["actionName"]}”。`;
+      } else if (
+        (party.some((value) => value.id === l["targetID"] && value.inParty) || l["targetID"] === youID) &&
+        keigenn[l["actionID"]] !== undefined
+      ) {
+        document
+          .querySelector("body > main > table > tbody")
+          .insertRow(-1)
+          .insertCell(0).innerHTML = `${duration} ${l["casterName"]}发动了“${l["actionName"]}”。`;
         document.querySelector("body > main").scrollTop = document.querySelector("body > main").scrollHeight;
       }
       break;
@@ -134,7 +184,9 @@ addOverlayListener("LogLine", (e) => {
       l = logProcessing(e.line, "status");
       if (
         keigenn[l["statusID"]] !== undefined &&
-        (party.some((value) => value.id === l["targetID"] && value.inParty) || l["targetID"] === youID || l["targetID"].substring(0, 1) === "4")
+        (party.some((value) => value.id === l["targetID"] && value.inParty) ||
+          l["targetID"] === youID ||
+          l["targetID"].substring(0, 1) === "4")
       ) {
         FFXIVObject[l["targetID"]] = FFXIVObject[l["targetID"]] || new FFObject(l["targetID"], l["targetName"]);
         FFXIVObject[l["targetID"]].Status[l["statusID"]] = { name: l["statusName"], caster: l["casterName"] };
@@ -144,7 +196,9 @@ addOverlayListener("LogLine", (e) => {
       l = logProcessing(e.line, "status");
       if (
         keigenn[l["statusID"]] !== undefined &&
-        (party.some((value) => value.id === l["targetID"] && value.inParty) || l["targetID"] === youID || l["targetID"].substring(0, 1) === "4")
+        (party.some((value) => value.id === l["targetID"] && value.inParty) ||
+          l["targetID"] === youID ||
+          l["targetID"].substring(0, 1) === "4")
       )
         try {
           delete FFXIVObject[l["targetID"]].Status[l["statusID"]];
@@ -155,7 +209,9 @@ addOverlayListener("LogLine", (e) => {
   }
 });
 startOverlayEvents();
-document.querySelector("main").onscroll = (e) => (scrollMove = document.querySelector("main").scrollHeight - 282 - e.target.scrollTop < 5);
+document.querySelector("main").onscroll = (e) => {
+  scrollMove = document.querySelector("main").scrollHeight - document.body.offsetHeight - e.target.scrollTop < 20;
+};
 document.querySelector("header").onclick = function () {
   let m = document.querySelector("main");
   if (m.style.opacity === "0") {
@@ -166,3 +222,15 @@ document.querySelector("header").onclick = function () {
     this.style.opacity = "1";
   }
 };
+function startCombat() {
+  inCombat = true;
+  clearTimeout(combatTimer);
+  let d = 0;
+  combatTimer = setInterval(() => {
+    duration = `${parseInt(++d / 60)
+      .toString()
+      .padStart(2, "0")}:${parseInt(d % 60)
+      .toString()
+      .padStart(2, "0")}`;
+  }, 1000);
+}
