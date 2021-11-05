f7ba5a4b755e7a882b8ba5f10fc57c2740f8b9c7
Souma <553469159@qq.com>
2021-11-05 23:21:58 +0800

    Update keigenn.js

--- ui/KeigennRecord2/data/keigenn.js
+++ ui/KeigennRecord2/data/keigenn.js
@@ -1,70 +1,127 @@
 /*
  * @Author: Souma
- * @LastEditTime: 2021-09-16 13:24:13
+ * @LastEditTime: 2021-11-05 23:21:48
  */
 let keigenn = {
-  "4a7": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //铁壁
-  "740": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //盾阵
-  "496": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //干预
-  "4a": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //预警
-  "498": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //武装
-  "52": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //神圣领域
-  "2df": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //原初的直觉
-  "8b3": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //原初的勇猛
-  "57": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //战栗
-  "59": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //复仇
-  "199": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //死斗
-  "49a": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //至黑之夜
-  "2ea": { dodge: 1, physics: 0, magic: 1, darkness: 0, heal: 1 }, //弃明投暗
-  "2eb": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //暗影墙
-  "766": { dodge: 1, physics: 0, magic: 1, darkness: 0, heal: 1 }, //暗黑布道
-  "32a": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //行尸走肉
-  "811": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //死而不僵
-  "730": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //石之心
-  "728": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //伪装
-  "72a": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //星云
-  "72f": { dodge: 1, physics: 0, magic: 1, darkness: 0, heal: 1 }, //光之心
-  "72c": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //超火流星
-  "751": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //节制
-  "12b": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //野战治疗阵
-  "13d": { dodge: 1, physics: 0, magic: 1, darkness: 0, heal: 1 }, //异想的幻光
-  "753": { dodge: 1, physics: 0, magic: 1, darkness: 0, heal: 1 }, //炽天的幻光
-  "351": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //命运之轮（日）
-  "4b6": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //命运之轮（夜）
+  "4a7": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //铁壁
+  "740": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //盾阵
+  "496": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //干预
+  "4a": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //预警
+  "498": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //武装
+  "52": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //神圣领域
+  "2df": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //原初的直觉
+  "8b3": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //原初的勇猛
+  "57": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //战栗
+  "59": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //复仇
+  "199": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //死斗
+  "49a": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //至黑之夜
+  "2ea": { dodge: 1, physics: 0, magic: 1, darkness: 0 }, //弃明投暗
+  "2eb": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //暗影墙
+  "766": { dodge: 1, physics: 0, magic: 1, darkness: 0 }, //暗黑布道
+  "32a": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //行尸走肉
+  "811": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //死而不僵
+  "730": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //石之心
+  "728": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //伪装
+  "72a": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //星云
+  "72f": { dodge: 1, physics: 0, magic: 1, darkness: 0 }, //光之心
+  "72c": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //超火流星
+  "751": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //节制
+  "12b": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //野战治疗阵
+  "13d": { dodge: 1, physics: 0, magic: 1, darkness: 0 }, //异想的幻光
+  "753": { dodge: 1, physics: 0, magic: 1, darkness: 0 }, //炽天的幻光
+  "351": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //命运之轮（日）
+  "4b6": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //命运之轮（夜）
 
-  "4d0": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //心眼
-  "49b": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //金刚极意
-  "78e": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //行吟
-  "79f": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //策动
-  "722": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //防守之桑巴
+  "4d0": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //心眼
+  "49b": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //金刚极意
+  "78e": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //行吟
+  "79f": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //策动
+  "722": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //防守之桑巴
 
-  "2d7": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //圣光幕帘（已触发）
-  "4c2": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //神祝祷
-  "5b1": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //摆脱
-  "129": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //鼓舞
-  "77d": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //炽天的幕帘
-  "345": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //黑夜领域
-  "758": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //天星冲日（夜）
-  "761": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //天星交错（夜）
-  "1e8": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //残影
-  "a8": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //魔罩
-  "790": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //防护障壁
-  "76a": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //残暴弹
+  "2d7": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //圣光幕帘（已触发）
+  "4c2": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //神祝祷
+  "5b1": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //摆脱
+  "129": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //鼓舞
+  "77d": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //炽天的幕帘
+  "345": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //黑夜领域
+  "758": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //天星冲日（夜）
+  "761": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //天星交错（夜）
+  "1e8": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //残影
+  "a8": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //魔罩
+  "790": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //防护障壁
+  "76a": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //残暴弹
 
-  "4b9": { dodge: 1, physics: 1, magic: 0, darkness: 0, heal: 1 }, //亲疏自行
-  "4a9": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //雪仇
-  "4ab": { dodge: 1, physics: 1, magic: 0, darkness: 0, heal: 1 }, //牵制
-  "4b3": { dodge: 1, physics: 0, magic: 1, darkness: 0, heal: 1 }, //昏乱
+  "4b9": { dodge: 1, physics: 1, magic: 0, darkness: 0 }, //亲疏自行
+  "4a9": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //雪仇
+  "4ab": { dodge: 1, physics: 1, magic: 0, darkness: 0 }, //牵制
+  "4b3": { dodge: 1, physics: 0, magic: 1, darkness: 0 }, //昏乱
 
   //青魔
-  "848": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //体力增加（捕食）
-  "6b3": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //腐臭（臭气）
-  "843": { dodge: 1, physics: 0, magic: 1, darkness: 0, heal: 1 }, //智力精神降低（魔法锤）
-  "9c4": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //龙之力
-  "6ba": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //超硬化
-  "9c0": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //玄结界
-  "847": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //仙人盾
-  "6b7": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //强力守护
-  "842": { dodge: 1, physics: 1, magic: 1, darkness: 1, heal: 1 }, //哥布防御
+  "848": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //体力增加（捕食）
+  "6b3": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //腐臭（臭气）
+  "843": { dodge: 1, physics: 0, magic: 1, darkness: 0 }, //智力精神降低（魔法锤）
+  "9c4": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //龙之力
+  "6ba": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //超硬化
+  "9c0": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //玄结界
+  "847": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //仙人盾
+  "6b7": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //强力守护
+  "842": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //哥布防御
+
+  //受伤减轻
+  "37": { dodge: 1, physics: 1, magic: 0, darkness: 0 }, //物理受伤减轻
+  "3b": { dodge: 1, physics: 0, magic: 1, darkness: 0 }, //魔法受伤减轻
+  "3f": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //受伤减轻
+  "14b": { dodge: 1, physics: 0, magic: 1, darkness: 0 }, //魔法受伤减轻
+  "15e": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //受伤减轻
+  "196": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //受伤减轻
+  "246": { dodge: 1, physics: 0, magic: 1, darkness: 0 }, //魔法受伤减轻
+  "247": { dodge: 1, physics: 1, magic: 0, darkness: 0 }, //物理受伤减轻
+  "258": { dodge: 1, physics: 0, magic: 1, darkness: 0 }, //魔法受伤减轻
+  "259": { dodge: 1, physics: 1, magic: 0, darkness: 0 }, //物理受伤减轻
+  "324": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //受伤减轻
+  "32c": { dodge: 1, physics: 0, magic: 1, darkness: 0 }, //魔法受伤减轻
+  "383": { dodge: 1, physics: 1, magic: 0, darkness: 0 }, //物理受伤减轻
+  "390": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //受伤减轻
+  "3a1": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //受伤减轻
+  "44c": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //受伤减轻
+  "4ca": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //受伤减轻
+  "609": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //受伤减轻
+  "6df": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //受伤减轻
+  "6f6": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //受伤减轻
+  "896": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //受伤减轻
+  "9ca": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //受伤减轻
+
+  //受伤加重
+  "38": { dodge: 1, physics: 1, magic: 0, darkness: 0 }, //物理受伤加重
+  "3c": { dodge: 1, physics: 0, magic: 1, darkness: 0 }, //魔法受伤加重
+  "40": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //受伤加重
+  "7e": { dodge: 1, physics: 1, magic: 0, darkness: 0 }, //物理受伤加重
+  "c8": { dodge: 1, physics: 1, magic: 0, darkness: 0 }, //物理受伤加重
+  "ca": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //受伤加重
+  "1bc": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //受伤加重
+  "1ed": { dodge: 1, physics: 1, magic: 0, darkness: 0 }, //物理受伤加重
+  "1ee": { dodge: 1, physics: 0, magic: 1, darkness: 0 }, //魔法受伤加重
+  "233": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //受伤加重
+  "27e": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //受伤加重
+  "291": { dodge: 1, physics: 1, magic: 0, darkness: 0 }, //物理受伤加重
+  "292": { dodge: 1, physics: 0, magic: 1, darkness: 0 }, //魔法受伤加重
+  "2b7": { dodge: 1, physics: 1, magic: 0, darkness: 0 }, //物理受伤加重
+  "2ca": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //受伤加重
+  "326": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //受伤加重
+  "37d": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //受伤加重
+  "3a6": { dodge: 1, physics: 1, magic: 0, darkness: 0 }, //物理受伤加重
+  "41e": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //受伤加重
+  "472": { dodge: 1, physics: 0, magic: 1, darkness: 0 }, //魔法受伤加重
+  "4b8": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //受伤加重
+  "57a": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //受伤加重
+  "584": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //受伤加重
+  "63d": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //受伤加重
+  "6fd": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //受伤加重
+  "735": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //受伤加重
+  "82a": { dodge: 1, physics: 1, magic: 0, darkness: 0 }, //物理受伤加重
+  "82b": { dodge: 1, physics: 0, magic: 1, darkness: 0 }, //魔法受伤加重
+  "84b": { dodge: 1, physics: 1, magic: 0, darkness: 0 }, //物理受伤加重
+  "8a5": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //受伤加重
+  "92b": { dodge: 1, physics: 1, magic: 1, darkness: 1 }, //受伤加重
 };
 export { keigenn };
