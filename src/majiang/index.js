3829d64aa7548abbec2405a19e16ab40b194c5ee
Souma <553469159@qq.com>
2022-01-06 05:42:15 +0800

    2022.1.6

--- src/majiang/index.js
+++ src/majiang/index.js
@@ -13,6 +13,9 @@ addOverlayListener("LogLine", (e) => handleLogLine(e));
 addOverlayListener("PartyChanged", (e) => handlePartyChanged(e.party));
 startOverlayEvents();
 
+(function init() {
+  if (!window.OverlayPluginApi) document.body.innerHTML = "请在ACT悬浮窗中添加此页面，而不是浏览器直接访问。";
+})();
 function handleChangePrimaryPlayer(e) {
   playerID = e.charID.toString(16).toUpperCase();
 }
